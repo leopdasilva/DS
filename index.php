22bf2e6df18d09bb62637d37cda40ee12efb4b51
Leo Silva <leonardopereiradasilva774@gmail.com>
2026-09-14 15:28:43 -0300

    index.php alterado com html

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-echo "<h1>Olá mundo!</h1>";
-echo "<p>Lorem ipsum dolor sit sat amet, consectetur adipiscing elit. Proin ac libero sit amet justo vulputate fermentum. Mauris nec risus id justo porttitor hendrerit. Donec et iaculis dui. Aliquam id neque ac massa dapibus vestibulum ut non nisl. Cras commodo tempor sem ac dictum.<p>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML & PHP</title>
+</head>
+<body>
+    <?php 
+        echo "<h1>Olá mundo no HTML!</h1>";
+        echo "<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit quo soluta aspernatur, animi necessitatibus omnis quis assumenda ratione. Obcaecati doloremque numquam nesciunt! Inventore nobis sit totam labore, magni velit doloribus.</p>";
+        echo "<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, nisi asperiores! Maxime, velit, cumque doloribus libero delectus similique, saepe dicta eius aspernatur qui inventore. Quibusdam placeat reiciendis quisquam voluptatem ullam!</p>";
+    ?>
+</body>
+</html>
