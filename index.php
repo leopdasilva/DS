b0e7f6fcef73d9f52f5ad1ec56e9cb22a9153f89
Leo Silva <leonardopereiradasilva774@gmail.com>
2026-09-14 15:23:40 -0300

    index.php atualizado

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php
 echo "<h1>Olá mundo!</h1>";
+echo "<p>Lorem ipsum dolor sit sat amet, consectetur adipiscing elit. Proin ac libero sit amet justo vulputate fermentum. Mauris nec risus id justo porttitor hendrerit. Donec et iaculis dui. Aliquam id neque ac massa dapibus vestibulum ut non nisl. Cras commodo tempor sem ac dictum.<p>";
