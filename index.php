e91de2d852741b21994776aab396c6e275f9c8b8
Leo Silva <leonardopereiradasilva774@gmail.com>
2026-09-15 16:45:51 -0300

    formulario

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     $idade = 0;
     $resultado = "";
 
-    if ($_SERVER["REQUEST_METHOD" == "POST"]) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $nome = $_POST["nome"];
         $idade = $_POST["idade"];
 
