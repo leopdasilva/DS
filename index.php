b547e0df1347ff75ea36342a029d54eaa96c2e2a
Leo Silva <leonardopereiradasilva774@gmail.com>
2026-09-15 17:15:34 -0300

    Aula 05 - 15-09-2026 - Página formulário
    Commits anteriores foram para vereficar o progresso ao alterar o código

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HTML & PHP</title>
+    <title>HTML & PHP - DS</title>
     <link rel="stylesheet" href="index.css">
 </head>
 
