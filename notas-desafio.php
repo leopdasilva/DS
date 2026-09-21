a98037c50edb65195d38210e066739695555b1b6
Leo Silva <leonardopereiradasilva774@gmail.com>
2026-09-21 16:47:37 -0300

    Atualizado notas-desafio.php e link para index.css

--- notas-desafio.php
+++ notas-desafio.php
@@ -35,7 +35,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Notas - HTML & PHP</title>
+    <title>Notas (Desafio) - HTML & PHP</title>
     <link rel="stylesheet" href="notas.css">
 </head>
 
