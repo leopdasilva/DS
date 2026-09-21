b4fa376aacf641815070d737ce1041de362edb08
Leo Silva <leonardopereiradasilva774@gmail.com>
2026-09-21 15:11:54 -0300

    Caminho para idade.css corrigido

--- idade.php
+++ idade.php
@@ -22,7 +22,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IDADE - HTML & PHP</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="idade.css">
 </head>
 
 <body>
