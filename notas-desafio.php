211ca83da2e97755fe19f54981ebab78e14b8492
Leo Silva <leonardopereiradasilva774@gmail.com>
2026-09-22 15:15:50 -0300

    Corrigido erro de sinal em notas.php

--- notas-desafio.php
+++ notas-desafio.php
@@ -20,7 +20,7 @@
         //Calculo de média
         $media = (($nota1 * 2) + ($nota2 * 3) + ($nota3 * 1) + ($nota4 * 1) + ($nota5 * 3)) / 10;
 
-        if ($media = 10) {
+        if ($media == 10) {
             $situacao = "Aprovado com exelência";
         } else if ($media >= 7) {
             $situacao = "Aprovado";
--- notas.php
+++ notas.php
@@ -21,7 +21,7 @@
         //Calculo de média
         $media = (($nota1 * 2) + ($nota2 * 3) + ($nota3 * 1) + ($nota4 * 1) + ($nota5 * 3)) / 10;
 
-        if ($media = 10 && $frequencia >= 75) {
+        if ($media == 10 && $frequencia >= 75) {
             $situacao = "Aprovado com exelência";
         } else if ($media >= 7 && $frequencia >= 75) {
             $situacao = "Aprovado";
